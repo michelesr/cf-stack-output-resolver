d9f89765a331137e1286332dc06c1a1df8874166
Michele Sorcinelli <michele.sorcinelli@citizensadvice.org.uk>
2017-09-11 17:39:35 +0100

    Apply PEP8

--- stack_output_resolver.py
+++ stack_output_resolver.py
@@ -5,6 +5,7 @@ from jinja2 import Template
 
 cf = boto3.resource('cloudformation')
 
+
 def get_stack_output(stack_name, key):
     iterator = filter(lambda x: x['OutputKey'] == key,
                       cf.Stack(stack_name).outputs)
@@ -14,12 +15,15 @@ def get_stack_output(stack_name, key):
             'Output for "{}" not found in stack "{}"'.format(key, stack_name))
     return res['OutputValue']
 
+
 def render_template(template):
-    return Template(template).render(stack_output = get_stack_output)
+    return Template(template).render(stack_output=get_stack_output)
+
 
 def main():
     with open(argv[1], 'r') as f:
         print(render_template(f.read()))
+
 
 if __name__ == '__main__':
     main()
